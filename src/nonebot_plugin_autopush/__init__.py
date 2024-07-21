19327a06333108cb272ce8b15769352d1519f071
XiaoDeng3386 <1744793737@qq.com>
2024-07-21 14:42:36 +0800

    完善插件元数据

--- src/nonebot_plugin_autopush/__init__.py
+++ src/nonebot_plugin_autopush/__init__.py
@@ -7,6 +7,9 @@ __plugin_meta__ = PluginMetadata(
     description="NoneBot2 自动状态推送",
     usage="向指定服务器推送 NoneBot2 状态",
     config=Config,
+    type="application",
+    homepage="https://github.com/This-is-XiaoDeng/nonebot-plugin-autopush",
+    supported_adapters=None
 )
 
 require("nonebot_plugin_apscheduler")
